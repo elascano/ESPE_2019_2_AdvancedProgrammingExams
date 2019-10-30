90a4a13e4e69b4a8eda5d6b8678d951dd523bb5e
dennisesandoval <dasandoval3@espe.edu.ec>
2019-10-30 14:01:14 -0500

    2 uris

--- Exam1/Sandoval/products/src/java/ec/edu/espe/school/services/ApplicationConfig.java
+++ Exam1/Sandoval/products/src/java/ec/edu/espe/school/services/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(ec.edu.espe.school.services.AllproductsResource.class);
         resources.add(ec.edu.espe.school.services.ProductbyidResource.class);
     }
     
