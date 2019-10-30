1fb1ec192a796c6280e2e321c8b4251124725b82
Diego Beltrán <diegoandres1198@gmail.com>
2019-10-30 13:46:01 -0500

    Code Service

---
+++ Exam1/Beltran/Code/Store1/src/java/Entity/Total.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author diego
+ */
+public class Total {
+        private float total;
+
+    public Total() {
+    }
+
+    
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+        
+}
