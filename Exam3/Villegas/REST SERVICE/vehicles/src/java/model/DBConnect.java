9c9b5c1c761c4dbc67cf8edff34d51e825db560f
Diego Villegas <38533871+dgvillegas@users.noreply.github.com>
2020-01-27 13:59:02 -0500

    Exam Villegas

---
+++ Exam3/Villegas/REST SERVICE/vehicles/src/java/model/DBConnect.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Dennise
+ */
+public class DBConnect {
+
+    Connection ct;
+    String bd = "database_vehicles";
+    //String url = "jdbc:mysql://localhost/" + bd;
+    String url = "jdbc:mysql://freigthtransport.ddns.net/" + bd;
+    String user = "root";
+    //String pass = "";
+    String pass = "11023650";
+
+    public Connection connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            ct = (Connection) DriverManager.getConnection(url, user, pass);
+            System.out.println("Successful connection");
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("ERROR");
+        }
+        return ct;
+    }
+
+    public boolean confirmConnect() {
+        boolean confirm;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            ct = (Connection) DriverManager.getConnection(url, user, pass);
+            confirm = true;
+        } catch (ClassNotFoundException | SQLException ex) {
+            confirm = false;
+        }
+        return confirm;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        DBConnect c = new DBConnect();
+        c.connect();
+
+    }
+}
