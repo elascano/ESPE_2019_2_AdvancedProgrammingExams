b5ef3494639467664a17bbc890900517c1687bf1
Santiago <santi.viv@hotmail.com>
2020-01-27 13:58:07 -0500

    Vivas

---
+++ Exam3/Vivas/REST SERVICE/vehicles/src/java/model/vehicle.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author DenniseSandoval
+ */
+public class vehicle {
+    private String id;
+    private String brand;
+    private String model;
+
+    public vehicle() {
+    }
+
+    public vehicle(String id, String brand, String model) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand the brand to set
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * @return the model
+     */
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * @param model the model to set
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+    
+}
